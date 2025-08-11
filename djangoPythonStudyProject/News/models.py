b967526b0e34e1d12a12a42f06489ec54ad91c47
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2025-08-11 22:32:03 +0300

    add database model

--- djangoPythonStudyProject/News/models.py
+++ djangoPythonStudyProject/News/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True, blank=True)
+    updateAt = models.DateTimeField(auto_now=True, blank=True)
+    photo = models.ImageField(upload_to='media/%Y/%m/%d', null=True, blank=True)
+    is_published = models.BooleanField(default=True)
