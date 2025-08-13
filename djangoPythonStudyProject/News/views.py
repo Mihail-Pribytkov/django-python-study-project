fa155879c55d6f4dae96d9c8a24d7915043cdfc9
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2025-08-13 21:16:29 +0300

    add news template

--- djangoPythonStudyProject/News/views.py
+++ djangoPythonStudyProject/News/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 
+from .models import News
+
 def index(request):
-    print(request)
-    return HttpResponse('<h1>Hello world<h1>')
+    news = News.objects.all()
+    res = '<h1>Список новостей<h1>'
+    for i in news:
+        res += f'<div>\n<p>{i.title}<p>\n<p>{i.content}<p></div>\n</div>\n<hr>\n'
+    return HttpResponse(res)
