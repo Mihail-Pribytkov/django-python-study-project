6937e169deb7be46cc7b95592fd770c36cd8abc3
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2025-08-18 19:51:19 +0300

    add admin panel

--- djangoPythonStudyProject/News/admin.py
+++ djangoPythonStudyProject/News/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import News
 
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'createdAt', 'updateAt']
+    list_display_links = ['title']
+    search_fields = ['title', 'content']
+
+
+
+admin.site.register(News, NewsAdmin)
 # Register your models here.
--- djangoPythonStudyProject/News/models.py
+++ djangoPythonStudyProject/News/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField(blank=True)
-    createdAt = models.DateTimeField(auto_now_add=True, blank=True)
-    updateAt = models.DateTimeField(auto_now=True, blank=True)
-    photo = models.ImageField(upload_to='media/%Y/%m/%d', null=True, blank=True)
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=150, verbose_name = 'Заголовок')
+    content = models.TextField(blank=True, verbose_name = 'Контент')
+    createdAt = models.DateTimeField(auto_now_add=True, blank=True, verbose_name = 'Время создания')
+    updateAt = models.DateTimeField(auto_now=True, blank=True, verbose_name = 'Время обновления')
+    photo = models.ImageField(upload_to='media/%Y/%m/%d', null=True, blank=True, verbose_name = 'Фото')
+    is_published = models.BooleanField(default=True, verbose_name = 'Публикована')
+
+    class Meta:
+        verbose_name = 'Новости'
+        verbose_name_plural = 'Новости'
+        ordering = ['-createdAt']
