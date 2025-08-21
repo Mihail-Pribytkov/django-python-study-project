952bd6443398d4886ec3b00b22c9c47709310c5e
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2025-08-21 21:26:43 +0300

    add new db model

--- djangoPythonStudyProject/News/models.py
+++ djangoPythonStudyProject/News/models.py
@@ -7,8 +7,18 @@ class News(models.Model):
     updateAt = models.DateTimeField(auto_now=True, blank=True, verbose_name = 'Время обновления')
     photo = models.ImageField(upload_to='media/%Y/%m/%d', null=True, blank=True, verbose_name = 'Фото')
     is_published = models.BooleanField(default=True, verbose_name = 'Публикация')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     class Meta:
         verbose_name = 'Новости'
         verbose_name_plural = 'Новости'
         ordering = ['-createdAt']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name = 'Категория')
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']
