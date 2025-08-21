7cd260495da2bd6589622e30e8bd480b15ff0cdf
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2025-08-21 21:38:24 +0300

    add connection db table model

--- djangoPythonStudyProject/News/admin.py
+++ djangoPythonStudyProject/News/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from .models import News
+from .models import News, Category
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ['title', 'createdAt', 'updateAt']
+    list_display = [ 'id', 'category', 'title', 'createdAt', 'updateAt']
     list_display_links = ['title']
     search_fields = ['title', 'content']
+    list_filter = ['is_published', 'id']
+    list_editable = [ 'category']
 
-
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title']
+    list_display_links = ['id', 'title']
 
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
 # Register your models here.
