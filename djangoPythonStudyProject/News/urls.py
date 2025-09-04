ea1c0e5b0b2b41b7ef8eaeba8deaa4b749de2b95
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2025-09-04 21:57:27 +0300

    .

--- djangoPythonStudyProject/News/urls.py
+++ djangoPythonStudyProject/News/urls.py
@@ -1,4 +1,6 @@
+from django.conf.urls.static import static
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
 
 from News.views import index
@@ -6,3 +8,6 @@ from News.views import index
 urlpatterns = [
     path('', index),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
