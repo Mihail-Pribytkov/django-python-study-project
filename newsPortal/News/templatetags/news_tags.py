8397d6da9e28bd9b87dd930dcd234761bbe09fcd
Mihail Pribytkov <mihail.pribytkov@yandex.ru>
2026-01-04 21:19:33 +0300

    add counter news in menu

--- newsPortal/News/templatetags/news_tags.py
+++ newsPortal/News/templatetags/news_tags.py
@@ -1,4 +1,5 @@
 from News.models import Category
+from django.db.models import Count
 from django import template
 
 register = template.Library()
@@ -9,5 +10,6 @@ def get_list_categories():
 
 @register.inclusion_tag('news/list_categories.html')
 def show_categories(arg1='Category', arg2='list'):
-    categories = Category.objects.all()
+    # categories = Category.objects.all()
+    categories = Category.objects.annotate(cnt=Count('news')).filter(cnt__gt=0)
     return {'categories': categories, 'arg1': arg1, 'arg2': arg2}
